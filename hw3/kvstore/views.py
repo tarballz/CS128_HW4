5a6b73c91cc5b970d3e303ee0678f9a0402cf22c
Payton <payton@ucsc.edu>
2017-11-20 00:33:54 -0800

    5 fails, 0 errors.  Some more cleanup

--- hw3/kvstore/views.py
+++ hw3/kvstore/views.py
@@ -16,16 +16,16 @@ from .NodeTracker import NodeTracker
 DEBUG = False
 
 # Environment variables.
-K          = int(os.getenv('K', 3))
-VIEW       = os.getenv('VIEW', "0.0.0.0:8080,10.0.0.20:8080,10.0.0.21:8080,10.0.0.22:8080")
-IPPORT     = os.getenv('IPPORT', "0.0.0.0:8080")
+K = int(os.getenv('K', 3))
+VIEW = os.getenv('VIEW', "0.0.0.0:8080,10.0.0.20:8080,10.0.0.21:8080,10.0.0.22:8080")
+IPPORT = os.getenv('IPPORT', "0.0.0.0:8080")
 current_vc = collections.OrderedDict()
 # AVAILIP = nodes that are up.
-AVAILIP    = {}
+AVAILIP = {}
 
-all_nodes     = []
+all_nodes = []
 replica_nodes = []
-proxy_nodes   = []
+proxy_nodes = []
 degraded_mode = False
 
 # if IPPORT != "0.0.0.0":
@@ -42,12 +42,12 @@ if VIEW != None:
         all_nodes = VIEW.split(',')
     for node in all_nodes:
         current_vc[node] = 0
-        AVAILIP[node]    = True
+        AVAILIP[node] = True
     if DEBUG:
         print(list(current_vc.values()))
     if len(VIEW) > K:
         replica_nodes = all_nodes[0:K]
-        proxy_nodes   = list(set(all_nodes) - set(replica_nodes))
+        proxy_nodes = list(set(all_nodes) - set(replica_nodes))
     else:
         degraded_mode = True
         replica_nodes = VIEW
@@ -72,6 +72,7 @@ def is_replica():
 def failure(request, key):
     return Response({'result': 'Error', 'msg': 'Key not valid'}, status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
 
+
 @api_view(['GET'])
 def get_node_details(request):
     if IPPORT in replica_nodes:
@@ -81,15 +82,17 @@ def get_node_details(request):
     else:
         return Response({"result": "error", "msg": "Node not found"}, status=status.HTTP_404_NOT_FOUND)
 
+
 @api_view(['GET'])
 def get_all_replicas(request):
     return Response({"result": "success", "replicas": replica_nodes}, status=status.HTTP_200_OK)
+
 
 # CORRECT KEYS
 @api_view(['GET', 'PUT'])
 def kvs_response(request, key):
     method = request.method
-    existing_entry     = None
+    existing_entry = None
     existing_timestamp = None
 
     # MAIN RESPONSE
@@ -99,30 +102,30 @@ def kvs_response(request, key):
             new_entry = False
             # ERROR HANDLING: INVALID KEY TYPE (NONE)
             if 'val' not in request.data:
-                return Response({'result':'Error','msg':'No value provided'},status=status.HTTP_400_BAD_REQUEST)
+                return Response({'result': 'Error', 'msg': 'No value provided'}, status=status.HTTP_400_BAD_REQUEST)
             input_value = request.data['val']
 
             # ERROR HANDLING: EMPTY VALUE or TOO LONG VALUE
             if 'val' not in request.data or sys.getsizeof(input_value) > 1024 * 1024 * 256:
-                return Response({'result':'Error','msg':'No value provided'},status=status.HTTP_400_BAD_REQUEST)
+                return Response({'result': 'Error', 'msg': 'No value provided'}, status=status.HTTP_400_BAD_REQUEST)
             # Maybe comment this out b/c causal payload can be '' in case if no reads have happened yet?
             if 'causal_payload' not in request.data:
-                return Response({'result': 'Error', 'msg': 'No causal_payload provided'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'result': 'Error', 'msg': 'No causal_payload provided'},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             # IF DATA HAS NODE_ID, THEN WE'VE RECEIVED NODE-TO-NODE COMMUNICATION
             # AND NEED TO STORE IT.
             if 'timestamp' in request.data:
                 # BUILD INCOMING OBJECT.
                 try:
-                    incoming_key       = str(request.data['key'])
-                    incoming_value     = str(request.data['val'])
-                    incoming_cp        = str(request.data['causal_payload'])
-                    incoming_node_id   = int(request.data['node_id'])
+                    #incoming_key = str(request.data['key'])
+                    incoming_value = str(request.data['val'])
+                    incoming_cp = str(request.data['causal_payload'])
+                    incoming_node_id = int(request.data['node_id'])
                     incoming_timestamp = int(request.data['timestamp'])
                 except:
                     return Response({'result': 'Error', 'msg': 'Cannot construct node-to-node entry'},
                                     status=status.HTTP_400_BAD_REQUEST)
-
 
                 cp_list = incoming_cp.split('.')
                 if DEBUG:
@@ -130,10 +133,10 @@ def kvs_response(request, key):
                 # IF INCOMING_CP > CURRENT_VC
                 if compare_vc(cp_list, list(current_vc.values())) == 1:
                     update_current_vc(incoming_cp)
-                    entry, created = Entry.objects.update_or_create(key=key, defaults={'val': incoming_value,
-                                                                                      'causal_payload': incoming_cp,
-                                                                                      'node_id': incoming_node_id,
-                                                                                      'timestamp': incoming_timestamp})
+                    Entry.objects.update_or_create(key=key, defaults={'val': incoming_value,
+                                                                      'causal_payload': incoming_cp,
+                                                                      'node_id': incoming_node_id,
+                                                                      'timestamp': incoming_timestamp})
                     return Response(
                         {'result': 'success', "value": incoming_value, "node_id": incoming_node_id,
                          "causal_payload": incoming_cp,
@@ -149,18 +152,18 @@ def kvs_response(request, key):
                         # FAILURE: KEY DOES NOT EXIST
                         # CREATE ENTRY IN OUR DB SINCE THE ENTRY DOESN'T EXIST.
                         Entry.objects.update_or_create(key=key, defaults={'val': incoming_value,
-                                                                         'causal_payload': incoming_cp,
-                                                                         'node_id': incoming_node_id,
-                                                                         'timestamp': incoming_timestamp})
+                                                                          'causal_payload': incoming_cp,
+                                                                          'node_id': incoming_node_id,
+                                                                          'timestamp': incoming_timestamp})
                         return Response({'result': 'Success', 'msg': 'Key does not exist'},
                                         status=status.HTTP_201_CREATED)
                     # IF WE'VE GOTTEN HERE, KEY EXISTS
                     else:
                         if incoming_timestamp > existing_entry.timestamp:
                             Entry.objects.update_or_create(key=key, defaults={'val': incoming_value,
-                                                                             'causal_payload': incoming_cp,
-                                                                             'node_id': incoming_node_id,
-                                                                             'timestamp': incoming_timestamp})
+                                                                              'causal_payload': incoming_cp,
+                                                                              'node_id': incoming_node_id,
+                                                                              'timestamp': incoming_timestamp})
                             return Response(
                                 {'result': 'success', "value": incoming_value, "node_id": incoming_node_id,
                                  "causal_payload": incoming_cp,
@@ -170,7 +173,7 @@ def kvs_response(request, key):
                                             status=status.HTTP_406_NOT_ACCEPTABLE)
 
                 # IF INCOMONG_CP < CURRENT_VC
-                #elif compare_vc(cp_list, list(current_vc.values())) == -1:
+                # elif compare_vc(cp_list, list(current_vc.values())) == -1:
                 else:
                     return Response({'result': 'failure', 'msg': 'Can\'t go back in time.'},
                                     status=status.HTTP_406_NOT_ACCEPTABLE)
@@ -178,10 +181,10 @@ def kvs_response(request, key):
 
             # IF NO TIMESTAMP, WE KNOW THIS PUT IS FROM THE CLIENT.
             else:
-                incoming_cp     = str(request.data['causal_payload'])
-                node_id         = list(current_vc.keys()).index(IPPORT)
-                new_timestamp   = int(time.time())
-                const_timestamp = new_timestamp # new_timestamp keeps updating :((
+                incoming_cp = str(request.data['causal_payload'])
+                node_id = list(current_vc.keys()).index(IPPORT)
+                new_timestamp = int(time.time())
+                const_timestamp = new_timestamp  # new_timestamp keeps updating :((
 
                 if DEBUG:
                     print("incoming_cp_CLIENT: %s" % (incoming_cp))
@@ -193,7 +196,7 @@ def kvs_response(request, key):
                     if DEBUG:
                         print("init triggered")
                     # Initialize vector clock.
-                    for k,v in current_vc.items():
+                    for k, v in current_vc.items():
                         if v is not None:
                             incoming_cp += str(v) + '.'
                     # STRIP LAST LETTER FROM INCOMING CP
@@ -220,8 +223,8 @@ def kvs_response(request, key):
                 existing_entry = None
                 try:
                     existing_entry = Entry.objects.get(key=key)
-                    #existing_entry = Entry.objects.latest('timestamp')
-                    existing_timestamp = existing_entry.timestamp
+                    # existing_entry = Entry.objects.latest('timestamp')
+                    # existing_timestamp = existing_entry.timestamp
                 except:
                     new_entry = True
 
@@ -235,12 +238,13 @@ def kvs_response(request, key):
                 # I SET THIS TO BE "> -1" B/C IT DOES NOT MATTER IF VCS ARE THE SAME B/C CLIENT WILL NOT PASS A TIMESTAMP
                 if compare_vc(cp_list, list(current_vc.values())) > -1:
                     # print ("OLD VC: %s" % (current_vc))
+                    # TODO: Somehow update incoming_cp to take on the += 1 that's applied to cp_list.
                     update_current_vc_client(cp_list)
 
-                    entry, created = Entry.objects.update_or_create(key=key, defaults={'val': input_value,
-                                                                                       'causal_payload': incoming_cp,
-                                                                                       'node_id': node_id,
-                                                                                       'timestamp': const_timestamp})
+                    Entry.objects.update_or_create(key=key, defaults={'val': input_value,
+                                                                      'causal_payload': incoming_cp,
+                                                                      'node_id': node_id,
+                                                                      'timestamp': const_timestamp})
                     return Response(
                         {'result': 'success', "value": input_value, "node_id": node_id, "causal_payload": incoming_cp,
                          "timestamp": const_timestamp}, status=status.HTTP_200_OK)
@@ -257,17 +261,19 @@ def kvs_response(request, key):
             try:
                 # KEY EXISTS
                 existing_entry = Entry.objects.get(key=key)
-                return Response({'result': 'success', "value": existing_entry.val, "node_id": existing_entry.node_id,"causal_payload": existing_entry.causal_payload,"timestamp": existing_entry.timestamp}, status=status.HTTP_200_OK)
+                return Response({'result': 'success', "value": existing_entry.val, "node_id": existing_entry.node_id,
+                                 "causal_payload": existing_entry.causal_payload,
+                                 "timestamp": existing_entry.timestamp}, status=status.HTTP_200_OK)
             except:
                 # ERROR HANDLING: KEY DOES NOT EXIST
-                return Response({'result':'Error','msg':'Key does not exist'},status=status.HTTP_404_NOT_FOUND)
+                return Response({'result': 'Error', 'msg': 'Key does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
     # PROXY RESPONSE
     else:
 
-    # 	# GENERATE BASE URL STRING
-    #     url_str = 'http://'+os.environ['MAINIP']+'/kv-store/'+key
+        # 	# GENERATE BASE URL STRING
+        #     url_str = 'http://'+os.environ['MAINIP']+'/kv-store/'+key
         # TODO: Implement some form of gossip or laziest_node() is useless.
         dest_node = laziest_node(current_vc)
         if DEBUG:
@@ -276,15 +282,14 @@ def kvs_response(request, key):
         # Some letters get chopped off when I forward.  Only retaining last letter..?
         url_str = 'http://' + dest_node + '/kv-store/' + key
 
-
-    # 	# FORWARD GET REQUEST
-    # 		# PACKAGE AND RETURN RESPONSE TO CLIENT
+        # 	# FORWARD GET REQUEST
+        # 		# PACKAGE AND RETURN RESPONSE TO CLIENT
         if method == 'GET':
             res = req.get(url=url_str, timeout=3)
             response = Response(res.json())
             response.status_code = res.status_code
-    # 	# MODIFY URL STRING WITH PUT INPUT AND FORWARD PUT REQUEST
-    # 		# PACKAGE AND RETURN RESPONSE TO CLIENT
+            # 	# MODIFY URL STRING WITH PUT INPUT AND FORWARD PUT REQUEST
+            # 		# PACKAGE AND RETURN RESPONSE TO CLIENT
         elif method == 'PUT':
             try:
                 res = req.put(url=url_str, data=request.data)
@@ -301,13 +306,14 @@ def broadcast(key, value, cp, node_id, timestamp):
         if dest_node != IPPORT:
             url_str = 'http://' + dest_node + '/kv-store/' + key
             try:
-                res = req.put(url=url_str, data={'key': key,
-                                                 'val': value,
-                                                 'causal_payload': cp,
-                                                 'node_id': node_id,
-                                                 'timestamp': timestamp})
+                req.put(url=url_str, data={'key': key,
+                                           'val': value,
+                                           'causal_payload': cp,
+                                           'node_id': node_id,
+                                           'timestamp': timestamp})
             except:
                 pass
+
 
 # Gross-ass way to update current_vc
 def update_current_vc(new_cp):
@@ -318,6 +324,7 @@ def update_current_vc(new_cp):
             i += 1
     if DEBUG:
         print("NEW 1VC: %s" % (current_vc))
+
 
 # Gross-ass way to update current_vc
 def update_current_vc_client(new_cp):
@@ -338,7 +345,7 @@ def update_current_vc_client(new_cp):
 def update_view(request):
     new_ipport = request.data['ip_port']
 
-    #print("TYPE IS: %s" % (str(request.GET.get('type'))))
+    # print("TYPE IS: %s" % (str(request.GET.get('type'))))
 
     if str(request.GET.get('type')) == 'add':
         # Added node should be a replica.
@@ -359,7 +366,8 @@ def update_view(request):
             degraded_mode = False
 
         return Response(
-            {"msg": "success", "node_id": list(current_vc.keys()).index(new_ipport), "number_of_nodes": len(all_nodes)}, status=status.HTTP_200_OK)
+            {"msg": "success", "node_id": list(current_vc.keys()).index(new_ipport), "number_of_nodes": len(all_nodes)},
+            status=status.HTTP_200_OK)
 
     elif str(request.GET.get('type')) == 'remove':
         all_nodes.remove(new_ipport)
@@ -389,12 +397,15 @@ def update_view(request):
 
 
     else:
-        return Response({'result': 'error', 'msg': 'key value store is not available'}, status=status.HTTP_501_NOT_IMPLEMENTED)
+        return Response({'result': 'error', 'msg': 'key value store is not available'},
+                        status=status.HTTP_501_NOT_IMPLEMENTED)
+
 
 @api_view(['GET'])
 def check_nodes():
     # new_ipport = request.data['ip_port']
     return Response(status=status.HTTP_200_OK)
+
 
 def compare_vc(a, b):
     """
@@ -428,9 +439,6 @@ def find_min():
             key = k
     return key
 
+
 def laziest_node(r_nodes):
     return min(r_nodes.items(), key=lambda x: x[1])[0]
-
-
-
-
