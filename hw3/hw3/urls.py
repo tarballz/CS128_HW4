70f22c631d412c5d12e2147144073bc38f5f59cb
payton <thenumber46853@gmail.com>
2017-11-21 21:13:45 -0800

    plz fucking work

--- hw3/hw3/urls.py
+++ hw3/hw3/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^kv-store/get_all_replicas', views.get_all_replicas),
     url(r'^kv-store/update-view$', views.update_view),
     url(r'^kv-store/check_nodes', views.check_nodes),
-    url(r'^kv-store/(?P<key>[a-zA-Z0-9_]{1,200})$', views.kvs_response),
-    url(r'^kv-store/(?P<key>[a-zA-Z0-9_]{201,})$', views.failure),
+    url(r'^kv-store/(?P<key>[a-zA-Z0-9_]){1,200}$', views.kvs_response),
+    url(r'^kv-store/(?P<key>[a-zA-Z0-9_]){201,}$', views.failure),
 ]
 
 
