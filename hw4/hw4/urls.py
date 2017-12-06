6d14edc8ef58c77d9ac8f82d82e15a03789b8210
payton <thenumber46853@gmail.com>
2017-12-05 16:56:18 -0800

    update_view() does not work

--- hw4/hw4/urls.py
+++ hw4/hw4/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url
 from django.contrib import admin
 from kvstore import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^kv-store/get_node_details', views.get_node_details),
     url(r'^kv-store/get_all_replicas', views.get_all_replicas),
+    url(r'^kv-store/object_broadcast_receive', views.object_broadcast_receive),
     url(r'^kv-store/update-view$', views.update_view),
+    url(r'^kv-store/update_view_receiver', views.update_view_receiver),
     url(r'^kv-store/check_nodes', views.check_nodes),
+    url(r'^kv-store/call_broadcast', views.call_broadcast),
     url(r'^kv-store/(?P<key>[a-zA-Z0-9_]{1,200})$', views.kvs_response),
     url(r'^kv-store/(?P<key>[a-zA-Z0-9_]{201,})$', views.failure),
 ]
-
 
--- hw4/kvstore/views.py
+++ hw4/kvstore/views.py
@@ -8,10 +8,13 @@ from rest_framework import status
 from .models import Entry
 import requests as req
 import hashlib
+import json
+
 
 def chunk_list(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
 
 # SET DEBUG TO True  IF YOU'RE WORKING LOCALLY
 # SET DEBUG TO False IF YOU'RE WORKING THROUGH DOCKER
@@ -19,10 +22,10 @@ DEBUG = False
 
 # Environment variables.
 K = int(os.getenv('K', 2))
-VIEW = os.getenv('VIEW', "0.0.0.0:8080,10.0.0.20:8080,10.0.0.21:8080,10.0.0.22:8080")
+VIEW = os.getenv('VIEW', "localhost:8080,localhost:8081")
 if DEBUG:
     print("VIEW is of type: %s" % (type(VIEW)))
-IPPORT = os.getenv('IPPORT', '0.0.0.0:8080')
+IPPORT = os.getenv('IPPORT', 'localhost:8080')
 current_vc = collections.OrderedDict()
 # AVAILIP = nodes that are up.
 AVAILIP = {}
@@ -40,36 +43,45 @@ if DEBUG:
         all_nodes = [VIEW]
 
 if not DEBUG:
-    all_nodes = VIEW.split(',')
+    if ',' in VIEW:
+        all_nodes = VIEW.split(',')
+    else:
+        all_nodes = [VIEW]
+
+for node in all_nodes:
+    current_vc[node] = 0
+    AVAILIP[node] = True
 
 if DEBUG:
     print("all_nodes: %s" % (all_nodes))
     print("len of all_n: %d" % (len(all_nodes)))
-    print(list(current_vc.values()))
+    # print(list(current_vc.values()))
     print("proxy_nodes: %s" % (proxy_nodes))
     print("len of prox_n: %d" % (len(proxy_nodes)))
     print("replica_nodes: %s" % (replica_nodes))
     print("len of rep_n: %d" % (len(replica_nodes)))
 
-
 # INITIAL NUMBER OF PARTITIONS
 num_groups = len(all_nodes) // K  # Integer division.
+if num_groups <= 0:
+    num_groups = 1
 num_replicas = len(all_nodes) - (len(all_nodes) % K)
 BASE = 2
-#POWER = 9
-#POWER = num_groups**2
-MAX_HASH_NUM = BASE**9
-
+# POWER = 9
+# POWER = num_groups**2
+MAX_HASH_NUM = BASE ** 9
 
 groups_dict = {}
+groups_sorted_list = []
 # range of accepted hashed keys for a group
 step = (MAX_HASH_NUM // num_groups)
 # initial upper
 upper_bound = step
 # list of lists of nodes and proxies
-chunked = chunk_list(all_nodes, K)
+chunked = None
 my_upper_bound = -1
 lower_bound = -1
+
 
 # for each list of nodes in our list of lists of IPPORTS
 def chunk_assign():
@@ -80,6 +92,7 @@ def chunk_assign():
     global num_groups
     global chunked
     global all_nodes
+    global groups_sorted_list
 
     num_groups = len(all_nodes) // K
     step = (MAX_HASH_NUM // num_groups)
@@ -91,7 +104,7 @@ def chunk_assign():
             print("chunk: %s" % (chunk))
         # if the current list is comprised of enough nodes
         # to be considered a fully functional group
-        #if len(chunk) >= K:
+        # if len(chunk) >= K:
         if len(chunk) == K:
             # we associate the list of IPPORTS with an upper bound
             groups_dict[upper_bound] = chunk
@@ -109,7 +122,7 @@ def chunk_assign():
                     # Need this to confirm a key is within our range, and not JUST less than our value.
                     lower_bound = upper_bound - step
                 # add node to our view
-                replica_nodes.append(node)
+                #replica_nodes.append(node)
             # increment the upper range for the next cluster of IPPORTS
             upper_bound += step
         else:
@@ -117,16 +130,18 @@ def chunk_assign():
             for node in chunk:
                 # so we add them the our proxies
                 proxy_nodes.append(node)
-chunk_assign()
 
+
+        # list of our group_dict sorted by the keys -- (key = upper bound) --
+        groups_sorted_list = [[k, groups_dict[k]] for k in sorted(groups_dict, key=int)]
+
+
+chunk_assign()
 
 # take care of inconsistent max allowable hash
 # that way a key doesn't get hashed out of range
 if MAX_HASH_NUM > upper_bound:
     MAX_HASH_NUM = upper_bound
-
-# list of our group_dict sorted by the keys -- (key = upper bound) --
-groups_sorted_list = [(k, groups_dict[k]) for k in sorted(groups_dict, key=int)]
 
 if DEBUG:
     print("g_s_l: %s" % (groups_sorted_list))
@@ -134,6 +149,7 @@ if DEBUG:
 for node in replica_nodes:
     current_vc[node] = 0
     AVAILIP[node] = True
+
 
 def is_replica():
     return (IPPORT in replica_nodes)
@@ -172,6 +188,8 @@ def kvs_response(request, key):
     if is_replica():
         # MAIN PUT
         if method == 'PUT':
+            if not DEBUG:
+                ping_nodes()
             new_entry = False
             # ERROR HANDLING: INVALID KEY TYPE (NONE)
             if 'val' not in request.data:
@@ -284,7 +302,7 @@ def kvs_response(request, key):
                                         status=status.HTTP_406_NOT_ACCEPTABLE)
                 else:
                     return Response({'msg': 'hashed key is not in my range.', 'my_upper_bound': my_upper_bound},
-                                status=status.HTTP_412_PRECONDITION_FAILED)
+                                    status=status.HTTP_412_PRECONDITION_FAILED)
 
 
             # =====================================================
@@ -356,8 +374,10 @@ def kvs_response(request, key):
                                                                           'node_id': node_id,
                                                                           'timestamp': new_timestamp})
                         return Response(
-                            {'result': 'success', "value": input_value, "node_id": node_id, "causal_payload": incoming_cp,
-                             "timestamp": new_timestamp, "my_upper_bound": my_upper_bound}, status=205)  # status.HTTP_201_CREATED
+                            {'result': 'success', "value": input_value, "node_id": node_id,
+                             "causal_payload": incoming_cp,
+                             "timestamp": new_timestamp, "my_upper_bound": my_upper_bound},
+                            status=205)  # status.HTTP_201_CREATED
 
                     # USER HAS DONE READS BEFORE
                     else:
@@ -407,10 +427,12 @@ def kvs_response(request, key):
                                             status=status.HTTP_406_NOT_ACCEPTABLE)
                 else:
                     return selective_broadcast(key, input_value, incoming_cp)
-                    #return Response({'msg': 'hashed key is not in my range.', 'my_upper_bound': my_upper_bound}, status=status.HTTP_412_PRECONDITION_FAILED)
+                    # return Response({'msg': 'hashed key is not in my range.', 'my_upper_bound': my_upper_bound}, status=status.HTTP_412_PRECONDITION_FAILED)
 
         # MAIN GET
         elif method == 'GET':
+            if not DEBUG:
+                ping_nodes()
             for entry in Entry.objects.all():
                 if DEBUG:
                     print("ENTRY INFO:")
@@ -439,7 +461,7 @@ def kvs_response(request, key):
 
         # 	# GENERATE BASE URL STRING
         #     url_str = 'http://'+os.environ['MAINIP']+'/kv-store/'+key
-        dest_node = laziest_node(current_vc)
+        dest_node = laziest_node(replica_nodes)
         if DEBUG:
             print("SELECTED ", dest_node, " TO FORWARD TO.")
 
@@ -468,6 +490,7 @@ def kvs_response(request, key):
 
 def broadcast(key, value, cp, node_id, timestamp, is_GET_broadcast):
     global AVAILIP
+
     for k in AVAILIP:
         # IF THE NODE IS UP, AND THE NODE IS NOT ME, AND WE'RE IN THE SAME GROUP
         if AVAILIP[k] and k != IPPORT:
@@ -481,9 +504,43 @@ def broadcast(key, value, cp, node_id, timestamp, is_GET_broadcast):
             except:
                 AVAILIP[k] = False
 
+
+def object_broadcast(entry_list):
+    global AVAILIP
+
+    for k in AVAILIP:
+        if AVAILIP[k] and k != IPPORT:
+            url_str = 'http://' + k + '/kv-store/object_broadcast_receive'
+            data = {'entry_list': entry_list}
+            try:
+                res = req.put(url=url_str, json=data, timeout=0.5)
+                response = Response(res.json())
+                response.status_code = res.status_code
+                return response
+            except:
+                AVAILIP[k] = False
+                continue
+    return Response({"msg": "shits fucked"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+@api_view(['PUT'])
+def object_broadcast_receive(request):
+    try:
+        entry_list = request.data['entry_list']
+        for entry in entry_list:
+            Entry.objects.update_or_create(key=entry.key, defaults={'val': entry.val,
+                                                                    'causal_payload': entry.causal_payload,
+                                                                    'node_id': entry.node_id,
+                                                                    'timestamp': entry.timestamp})
+        return Response({"msg": "it worked."}, status=status.HTTP_201_CREATED)
+    except:
+        return Response({"msg": "its fucked"}, status=status.HTTP_502_BAD_GATEWAY)
+
+
 def selective_broadcast(key, value, cp):
     sh = seeded_hash(key)
-    for k,v in groups_sorted_list:
+    for k, v in groups_sorted_list:
         if sh <= k:
             for dest_node in v:
                 try:
@@ -504,6 +561,7 @@ def i_should_store(key):
         print("hashed key: %s" % (sh))
         print("my_ub: %s" % (my_upper_bound))
     return (sh > lower_bound and sh <= my_upper_bound)
+
 
 # Gross-ass way to update current_vc
 def update_current_vc(new_cp):
@@ -537,6 +595,7 @@ def update_current_vc_client(new_cp):
 
 def ping_nodes():
     global AVAILIP
+
     for k in AVAILIP:
         if repr(k) != IPPORT:
             if DEBUG:
@@ -566,6 +625,8 @@ def ping_nodes():
                         replica_nodes.remove(k)
 
 
+# THIS FUNCTION WILL CREATE A NEW VIEW-OF-THE-WORLD AND MODIFY OUR LISTS OF NODES IN ACCORDANCE TO
+# HOW MANY NODES WE HAVE (len(all_nodes)) AND HOW MANY NODES ARE IN A CLUSTER.
 @api_view(['PUT'])
 def update_view(request):
     global replica_nodes
@@ -573,12 +634,13 @@ def update_view(request):
     global all_nodes
     global current_vc
     global AVAILIP
+    global chunked
+    global groups_sorted_list
 
     new_ipport = request.data['ip_port']
     # ping_nodes()
     node_num = 0
     # print("TYPE IS: %s" % (str(request.GET.get('type'))))
-    # TODO: Call update_view_pusher(new_ip) somewhere clever.
 
     if request.GET.get('type') == 'add':
         node_num = 0
@@ -591,35 +653,32 @@ def update_view(request):
         # Added node should be a replica.
         all_nodes.append(new_ipport)
         AVAILIP[new_ipport] = True
-        if len(replica_nodes) < K:
+        if new_ipport not in current_vc:
+            # Init new entry into our dictionary.
+            current_vc.update({new_ipport: 0})
             if DEBUG:
-                print("\t\t\tIN DEGRADED MODE\nAPPENDING %s ONTO R_N LIST" % (new_ipport))
-            replica_nodes.append(new_ipport)
-            # Check if we're resurrecting a node that was previously in our OrderedDict current_vc
-            if new_ipport not in current_vc:
-                # Init new entry into our dictionary.
-                current_vc.update({new_ipport: 0})
-                if DEBUG:
-                    print("\t\t\tUPDATING CURRENT_VC")
-                    print("current_vc: %s" % (current_vc.items()))
-                    print("%s should be in current_vc" % (new_ipport))
-        # Added node should be a proxy
-        elif len(replica_nodes) >= K:
-            if DEBUG:
-                print("K = %d \t len(r_n) = %d" % (K, len(replica_nodes)))
-                print("APPENDING %s onto proxy_nodes" % (new_ipport))
-            proxy_nodes.append(new_ipport)
-            degraded_mode = False
+                print("\t\t\tUPDATING CURRENT_VC")
+                print("current_vc: %s" % (current_vc.items()))
+                print("%s should be in current_vc" % (new_ipport))
+
+        for k in AVAILIP:
+            if AVAILIP[k]:
+                node_num += 1
+
+        replica_nodes = []
+        proxy_nodes = []
+        # REEVALUATE OUR UPPERBOUND AND RE-CHUNK OUR NODES.
+        chunk_assign()
+        # SEND OUR NEW VIEW-OF-THE-WORLD TO ALL OTHER NODES.
+        update_view_pusher()
+
         if DEBUG:
             print("\t\t\tFor update_view after add")
             print("proxy_nodes: %s" % (proxy_nodes))
             print("len of prox_n: %d" % (len(proxy_nodes)))
             print("replica_nodes: %s" % (replica_nodes))
             print("len of rep_n: %d" % (len(replica_nodes)))
-
-        for k in AVAILIP:
-            if AVAILIP[k]:
-                node_num += 1
+            print("all_nodes: %s" % (all_nodes))
 
         return Response(
             {"msg": "success", "node_id": list(current_vc.keys()).index(new_ipport),
@@ -628,32 +687,39 @@ def update_view(request):
 
     elif request.GET.get('type') == 'remove':
         node_num = 0
-        # all_nodes.remove(new_ipport)
-        AVAILIP[new_ipport] = False
-        if new_ipport in replica_nodes:
-            replica_nodes.remove(new_ipport)
-            current_vc[new_ipport] = 0
-            if len(replica_nodes) < K:
-                # If we have any "spare" nodes in proxy_nodes, promote it to a replica.
-                if len(proxy_nodes) > 0:
-                    promoted = proxy_nodes.pop()
-                    replica_nodes.append(promoted)
-                    if promoted not in current_vc:
-                        current_vc.update({promoted: 0})
-                    else:
-                        current_vc[promoted] = 0
-
-                    if len(replica_nodes) > K:
-                        degraded_mode = False
-                    else:
-                        degraded_mode = True
-
-        elif new_ipport in proxy_nodes:
-            proxy_nodes.remove(new_ipport)
+        all_nodes.remove(new_ipport)
+        del AVAILIP[new_ipport]
+        # if new_ipport in replica_nodes:
+        #     replica_nodes.remove(new_ipport)
+        #     current_vc[new_ipport] = 0
+        #     if len(replica_nodes) < K:
+        #         # If we have any "spare" nodes in proxy_nodes, promote it to a replica.
+        #         if len(proxy_nodes) > 0:
+        #             promoted = proxy_nodes.pop()
+        #             replica_nodes.append(promoted)
+        #             if promoted not in current_vc:
+        #                 current_vc.update({promoted: 0})
+        #             else:
+        #                 current_vc[promoted] = 0
+        #
+        #             if len(replica_nodes) > K:
+        #                 degraded_mode = False
+        #             else:
+        #                 degraded_mode = True
+        #
+        # elif new_ipport in proxy_nodes:
+        #     proxy_nodes.remove(new_ipport)
 
         for k in AVAILIP:
             if AVAILIP[k]:
                 node_num += 1
+
+        replica_nodes = []
+        proxy_nodes = []
+        # REEVALUATE OUR UPPERBOUND AND RE-CHUNK OUR NODES.
+        chunk_assign()
+        # SEND OUR NEW VIEW-OF-THE-WORLD TO ALL OTHER NODES.
+        update_view_pusher()
 
         return Response(
             {"msg": "success", "node_id": list(current_vc.keys()).index(new_ipport),
@@ -663,30 +729,88 @@ def update_view(request):
     return Response({'result': 'error', 'msg': 'key value store is not available'},
                     status=status.HTTP_501_NOT_IMPLEMENTED)
 
-def update_view_pusher(cond, new_ip):
-    for dest_node in all_nodes:
-        if dest_node != IPPORT:
-            url_str = 'http://' + dest_node + '/kv-store/update_view_receiver'
-            try:
-                res = req.put(url=url_str, data={'type': cond,
-                                                 'AN': all_nodes,
-                                                 'AIP': AVAILIP,
-                                                 'GSL': groups_sorted_list})
-                #response = Response(res.json())
-                #response.status_code = res.status_code
-            except Exception:
-                continue
-                #AVAILIP[dest_node] = False
-                #return Response({'result': 'error', 'msg': 'Server unavailable'}, status=501)
 
+# THIS FUNCTION WILL PACKAGE OUR all_nodes list, AVAILIP list, and groups_sorted_list,
+# PACKAGE THEM UP AND SEND THEM TO EVERY NODE.  THESE NODES WILL THEN ACCEPT THESE LISTS
+# AND ACCEPT THEM AS THE NEW VIEW-OF-THE-WORLD IN update_view_receiver()
+def update_view_pusher():
+    if not DEBUG:
+        for dest_node in all_nodes:
+            if dest_node != IPPORT and AVAILIP[dest_node]:
+                url_str = 'http://' + dest_node + '/kv-store/update_view_receiver'
+                data = {'AN': all_nodes,
+                        'AIP': AVAILIP,
+                        'GSL': groups_sorted_list}
+                try:
+                    # HAD TO DO JSON= INSTEAD OF DATA= BC WE'RE PASSING A COMPLICATED STRUCTURE
+                    res = req.put(url=url_str, json=data)
+                    # response = Response(res.json())
+                    # response.status_code = res.status_code
+                except Exception:
+                    AVAILIP[dest_node] = False
+                    continue
+                    # return Response({'result': 'error', 'msg': 'Server unavailable'}, status=501)
+        for dest_node in all_nodes:
+            url_str = 'http://' + dest_node + '/kv-store/call_broadcast'
+            req.put(url=url_str, data=None)
+            # TODO: Some kind of prune?
+
+    else:  # if DEBUG:
+        url_str = 'http://' + "0.0.0.0:8080" + '/kv-store/update_view_receiver'
+        data = {'AN': all_nodes,
+                'AIP': AVAILIP,
+                'GSL': groups_sorted_list}
+
+        for dest_node in all_nodes:
+            url_str = 'http://' + dest_node + '/kv-store/call_broadcast'
+            req.put(url=url_str, data=None)
+
+        try:
+            res = req.put(url=url_str, json=data)
+            # response = Response(res.json())
+            # response.status_code = res.status_code
+        except Exception:
+            pass
+
+
+# SET MY LISTS TO THE NEW VIEW-OF-THE-WORLD
 @api_view(['PUT'])
 def update_view_receiver(request):
-    pass
+    global all_nodes
+    global AVAILIP
+    global groups_sorted_list
+    global my_upper_bound
+    global lower_bound
+
+    try:
+        new_all_nodes = request.data['AN']
+        new_AVAILIP = request.data['AIP']
+        new_gsl = request.data['GSL']
+
+        if DEBUG:
+            print("new_an: %s" % (new_all_nodes))
+            print("new_AVAIL: %s" % (new_AVAILIP))
+            print("new_gsl: %s" % (new_gsl))
+
+        all_nodes = new_all_nodes
+        AVAILIP = new_AVAILIP
+        groups_sorted_list = new_gsl
+
+        return Response({'msg': 'shits totally not fucked'}, status=200)
+
+    except:
+        return Response({'msg': 'shits fucked'}, status=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS)
+
 
 @api_view(['GET'])
 def check_nodes(request):
     # new_ipport = request.data['ip_port']
     return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+def call_broadcast(request):
+    return object_broadcast(Entry.objects.all())
 
 
 def compare_vc(a, b):
@@ -723,15 +847,16 @@ def find_min():
 
 
 def laziest_node(r_nodes):
-    return min(r_nodes.items(), key=lambda x: x[1])[0]
+    for node in replica_nodes:
+        if node != IPPORT:
+            return node
+    #return min(r_nodes.items(), key=lambda x: x[1])[0]
 
 
-#def key_to_group_hash(str):
+# def key_to_group_hash(str):
 #    return hash(str) % num_groups
 
 def seeded_hash(str):
     str = str.encode('utf-8')
     return int(hashlib.sha1(str).hexdigest(), 16) % MAX_HASH_NUM
-    #return hash(str) % MAX_HASH_NUM
-
-
+    # return hash(str) % MAX_HASH_NUM
